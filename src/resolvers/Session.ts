c25a68e3a947cdab33232313f8f90ac38838b854
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2021-07-06 06:31:36 +0530

    Throw custom error

--- src/resolvers/Session.ts
+++ src/resolvers/Session.ts
@@ -1,12 +1,20 @@
+import { ApolloError } from "apollo-server";
+
 export const Session = {
   async speakers(session: any, args: any, { dataSources }: any) {
-    const speakers = await dataSources.speakerAPI.getSpeakers();
-    return speakers.filter((speaker: any) =>
-      Boolean(
-        session.speakers.find(
-          (sessionSpeaker: any) => sessionSpeaker.id === speaker.id
+    try {
+      const speakers = await dataSources.speakerAPI.getSpeakers();
+      return speakers.filter((speaker: any) =>
+        Boolean(
+          session.speakers.find(
+            (sessionSpeaker: any) => sessionSpeaker.id === speaker.id
+          )
         )
-      )
-    );
+      );
+    } catch (error) {
+      return new ApolloError("Unable to get speakers", "SPEAKER_API_ERROR", {
+        token: "uniqueToken",
+      });
+    }
   },
 };
